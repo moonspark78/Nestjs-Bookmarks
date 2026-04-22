3b722ea6e33f657e0dc9cd93503b7015cfc23d0f
Omar Jabour <jabour.omar@hotmail.com>
2026-04-22 22:52:55 +0200

    create docker file & prisma model fix bug .

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,8 +1,10 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
 
 
 @Injectable()
 export class AuthService {
+    constructor(private prisma: PrismaService) {}
     signin() {
         return { message: "I am signing in" };
     }
