3bfa49d02ef536ad1cea7af2787c0ba81e2796b3
Omar Jabour <jabour.omar@hotmail.com>
2026-04-25 21:11:47 +0200

    Working on AuthController .

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,11 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true, // Supprime les propriétés qui n'ont pas de décorateurs dans le DTO
+  forbidNonWhitelisted: true, // Rejette la requête si des propriétés inconnues sont présentes
+  transform: true, // Transforme les objets JS en instances de classes DTO
+  }));
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
