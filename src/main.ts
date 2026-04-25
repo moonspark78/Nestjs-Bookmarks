6f712ef9876351bf844d23cd8a1a8f8d2f2f2f16
Omar Jabour <jabour.omar@hotmail.com>
2026-04-25 21:23:10 +0200

    Working on AuthController

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true, // Supprime les propriétés qui n'ont pas de décorateurs dans le DTO
-  forbidNonWhitelisted: true, // Rejette la requête si des propriétés inconnues sont présentes
-  transform: true, // Transforme les objets JS en instances de classes DTO
+    forbidNonWhitelisted: true, // Rejette la requête si des propriétés inconnues sont présentes
+    transform: true, // Transforme les objets JS en instances de classes DTO
   }));
   await app.listen(process.env.PORT ?? 3000);
 }
