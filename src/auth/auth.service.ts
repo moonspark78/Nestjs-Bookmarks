a02c6fc8fe4d1fbdfdb53bcb7d3b84f23f46933e
Omar Jabour <jabour.omar@hotmail.com>
2026-04-25 21:43:05 +0200

    Working on AuthController ././

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,15 +1,17 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
+import type { AuthDto } from './dto';
 
 
 @Injectable()
 export class AuthService {
     constructor(private prisma: PrismaService) {}
+    signup(dto: AuthDto) {
+        return { message: "I am signing up" };
+    }
+    
     signin() {
         return { message: "I am signing in" };
     }
 
-    signup() {
-        return { message: "I am signing up" };
-    }
 }
