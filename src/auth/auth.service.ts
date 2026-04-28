28ee03b7a6e444a4c7e73a3d31633c5e33d6eb8d
Omar Jabour <jabour.omar@hotmail.com>
2026-04-28 20:27:37 +0200

    modify the  auth.Service  try catch

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -42,6 +42,7 @@ export class AuthService {
     async signin() {
         // find the user by email
         //if user does not exist throw exception
+        // compare the password 
         return { message: "I am signing in" };
     }
 
