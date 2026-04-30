cb67737ba4d3cf582eb1c31990c1a06ef41e4f09
Omar Jabour <jabour.omar@hotmail.com>
2026-04-30 18:20:44 +0200

    :closed_lock_with_key:  Create the Strategy bug   :closed_lock_with_key:

--- src/auth/strategy/jwt.strategy.ts
+++ src/auth/strategy/jwt.strategy.ts
@@ -1,1 +1,11 @@
-export class JwtStrategy {}
+import { PassportStrategy } from "@nestjs/passport";
+import { ExtractJwt, Strategy } from "passport-jwt";
+
+export class JwtStrategy extends PassportStrategy(Strategy) {
+    constructor() {
+        super({
+            jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+            secretOrKey: jwtConstants.secret,
+        });
+    }
+}
