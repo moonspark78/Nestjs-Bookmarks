86e6250e5cb2d591ad3db0330f80191dc0845bcf
Omar Jabour <jabour.omar@hotmail.com>
2026-04-30 16:26:40 +0200

    AuthService ./

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
-import { PrismaModule } from '../prisma/prisma.module';
+import { JwtModule } from '@nestjs/jwt';
+
 @Module({
+  imports: [JwtModule],
   providers: [AuthService],
   controllers: [AuthController]
 })
