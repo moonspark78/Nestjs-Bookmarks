c52064d2ca53e34bcaab6df0325eeb58208cf4bc
Omar Jabour <jabour.omar@hotmail.com>
2026-05-01 19:24:46 +0200

    :closed_lock_with_key: Working on  the decoratorc.   :closed_lock_with_key:

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -3,9 +3,9 @@ import { JwtGuard } from '../auth/guard';
 import { GetUser } from '../auth/decorator';
 import type { User } from '../generated/prisma/client';
 
+@UseGuards(JwtGuard)
 @Controller('users')
 export class UserController {
-    @UseGuards(JwtGuard)
     @Get('me')
     getMe(@GetUser() user: User) {
         return user;
