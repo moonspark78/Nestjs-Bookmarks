dd3d03837132cec811f73785d40dda7428f4b255
Omar Jabour <jabour.omar@hotmail.com>
2026-05-01 16:54:57 +0200

    :closed_lock_with_key:  Working on  a controller user ./.  :closed_lock_with_key:

--- src/auth/strategy/jwt.strategy.ts
+++ src/auth/strategy/jwt.strategy.ts
@@ -21,6 +21,7 @@ export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
                 id: payload.sub,
             },
         });
-        return payload;
+        delete user.hash;
+        return user;
     }
 }
